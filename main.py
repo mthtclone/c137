5194d4903c0523b8d0197d4f3500166235e6d1be
mthtclone <morrg25@gmail.com>
2026-06-24 21:57:15 +0630

    folder structure and prep CODEOWNERS

---
+++ main.py
@@ -0,0 +1,30 @@
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+
+
+class Prototype(ShowBase):
+    def __init__(self):
+        super().__init__()
+
+        # Load a built-in Panda3D model
+        self.model = self.loader.loadModel("models/environment")
+        self.model.reparentTo(self.render)
+        self.model.setScale(0.1)
+        self.model.setPos(-8, 42, 0)
+
+        # Camera position
+        self.camera.setPos(0, -20, 5)
+        self.camera.lookAt(0, 0, 0)
+
+        # Rotate the world
+        self.taskMgr.add(self.spin_world, "spin_world")
+
+    def spin_world(self, task):
+        self.model.setH(task.time * 10)
+        return Task.cont
+
+
+app = Prototype()
+app.run()
+
+## The above code is just mere sample.
