264ac31c49c3ae09852c8697142340de51993fd4
mthtclone <morrg25@gmail.com>
2026-07-18 01:41:44 +0630

    a working model and render pipeline

--- engine/managers/level_manager.py
+++ engine/managers/level_manager.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from panda3d.core import TextNode
+
 from engine.managers.metadata_manager import MetadataManager
 
 
@@ -19,8 +21,16 @@ class LevelManager:
         print(f"Loading level: {level_folder}")
         print("=" * 50)
 
+        #
+        # Clear previous level
+        #
+
         self.unload_level()
         self.metadata.unload()
+
+        #
+        # Load metadata
+        #
 
         metadata_file = level_folder / "level_metadata.json"
 
@@ -29,25 +39,79 @@ class LevelManager:
 
             return False
 
+        #
+        # Find model
+        #
+
+        model_file = self.find_level_model(level_folder)
+
+        #
+        # Load model
+        #
+
+        if model_file is not None:
+            print("[LevelManager] Loading model...")
+
+            self.current_level = self.base.loader.loadModel(str(model_file))
+
+            self.current_level.reparentTo(self.base.render)
+
+            print("[LevelManager] Model loaded:")
+            print(f"    {model_file.name}")
+
+        else:
+            print("[LevelManager] WARNING")
+            print("    No level model found.")
+
+            self.current_level = self.create_placeholder()
+
         self.current_level_path = level_folder
 
-        print()
+        #
+        # Level information
+        #
 
         info = self.metadata.get_level()
 
+        print()
         print("Level Information")
         print("-----------------")
-
         print(f"Name: {info.get('name')}")
-
         print(f"Version: {info.get('version')}")
-
         print()
 
         return True
+
+    def find_level_model(self, level_folder):
+        #
+        # Preferred order
+        #
+
+        supported_models = ["level_model.glb", "level_model.gltf", "level_model.bam"]
+
+        for filename in supported_models:
+            path = level_folder / filename
+
+            if path.exists():
+                return path
+
+        return None
 
     def unload_level(self):
         if self.current_level:
             self.current_level.removeNode()
 
             self.current_level = None
+
+    def create_placeholder(self):
+        text = TextNode("missing_model")
+
+        text.setText("Missing level model")
+
+        node = self.base.render.attachNewNode(text)
+
+        node.setScale(2)
+
+        node.setPos(0, 20, 5)
+
+        return node
