56f61b6abbdac5772db5c845334f4e7d3e0ba773
mthtclone <morrg25@gmail.com>
2026-09-02 13:03:23 +0630

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,71 @@
+from setuptools import setup
+
+setup(
+    name="CopenhagenTrip",
+    version="0.1.0",
+    options={
+        "build_apps": {
+            # 1. Add your main script file name here
+            "gui_apps": {
+                "CopenhagentTrip": "main.py",
+            },
+            # 2. Add all your asset extensions and folders here
+            "include_patterns": [
+                "**/*.png",
+                "**/*.*",
+                "**/*.jpg",
+                "**/*.egg",
+                "**/*.bam",
+                "**/*.gltf",
+                "**/*.gltf.rpc",
+                "**/*.txt",
+            ],
+            # 3. List the internal Panda3D plugins you require
+            "plugins": [
+                "pandagl",  # OpenGL renderer (essential)
+                "p3openal_audio",  # Audio player (essential for sound)
+            ],
+            # 4. List your third-party pip packages here
+            "platforms": ["win_amd64"],  # Builds for 64-bit Windows
+            "include_modules": [
+                "gltf",
+                "simplepbr" "black",
+                "certifi",
+                "cfgv",
+                "charset-normalizer",
+                "click",
+                "colorama",
+                "distlib",
+                "filelock",
+                "identify",
+                "idna",
+                "mypy_extensions",
+                "nodeenv",
+                "numpy",
+                "packaging",
+                "Panda3D",
+                "panda3d-blend2bam",
+                "panda3d-gltf",
+                "panda3d-simplepbr",
+                "pathspec",
+                "pillow",
+                "platformdirs",
+                "pre_commit",
+                "pyquaternion",
+                "python-discovery",
+                "pytokens",
+                "PyYAML",
+                "requests",
+                "ruff",
+                "setuptools",
+                "typing_extensions",
+                "urllib3",
+                "virtualenv",
+            ],
+        }
+    },
+)
+
 # from setuptools import setup, find_packages
 
 
--- ui/loading_screen.py
+++ ui/loading_screen.py
@@ -7,9 +7,7 @@ from panda3d.core import TextNode
 
 
 class LoadingScreen:
-
     def __init__(self, parent, game):
-
         self.parent = parent
         self.game = game
 
@@ -22,21 +20,18 @@ class LoadingScreen:
 
         self.background = DirectFrame(
             parent=self.parent,
-
             frameSize=(
                 -2,
                 2,
                 -1.2,
                 1.2,
             ),
-
             frameColor=(
                 0.12,
                 0.12,
                 0.12,
                 1,
             ),
-
             relief=0,
         )
 
@@ -46,26 +41,20 @@ class LoadingScreen:
 
         self.loading_text = DirectLabel(
             parent=self.background,
-
             text="LOADING",
-
             pos=(
                 0,
                 0,
                 0.35,
             ),
-
             scale=0.07,
-
             text_fg=(
                 0.9,
                 0.9,
                 0.9,
                 1,
             ),
-
             text_align=TextNode.ACenter,
-
             relief=None,
         )
 
@@ -75,40 +64,32 @@ class LoadingScreen:
 
         self.progress_bar = DirectWaitBar(
             parent=self.background,
-
             pos=(
                 0,
                 0,
                 0.05,
             ),
-
             scale=0.8,
-
             value=0,
-
             range=100,
-
             frameSize=(
                 -1,
                 1,
                 -0.06,
                 0.06,
             ),
-
             frameColor=(
                 0.08,
                 0.08,
                 0.08,
                 1,
             ),
-
             barColor=(
                 0.75,
                 0.75,
                 0.75,
                 1,
             ),
-
             relief=0,
         )
 
@@ -118,26 +99,20 @@ class LoadingScreen:
 
         self.percent_text = DirectLabel(
             parent=self.background,
-
             text="0%",
-
             pos=(
                 0,
                 0,
                 -0.12,
             ),
-
             scale=0.045,
-
             text_fg=(
                 0.65,
                 0.65,
                 0.65,
                 1,
             ),
-
             text_align=TextNode.ACenter,
-
             relief=None,
         )
 
@@ -147,26 +122,20 @@ class LoadingScreen:
 
         self.status_text = DirectLabel(
             parent=self.background,
-
             text="Preparing...",
-
             pos=(
                 0,
                 0,
                 -0.38,
             ),
-
             scale=0.038,
-
             text_fg=(
                 0.55,
                 0.55,
                 0.55,
                 1,
             ),
-
             text_align=TextNode.ACenter,
-
             relief=None,
         )
 
@@ -177,7 +146,6 @@ class LoadingScreen:
     # ======================================================
 
     def set_progress(self, value, status=None):
-
         self.progress = max(
             0,
             min(100, value),
@@ -185,9 +153,7 @@ class LoadingScreen:
 
         self.progress_bar["value"] = self.progress
 
-        self.percent_text["text"] = (
-            f"{int(self.progress)}%"
-        )
+        self.percent_text["text"] = f"{int(self.progress)}%"
 
         if status is not None:
             self.status_text["text"] = status
@@ -200,7 +166,6 @@ class LoadingScreen:
     # ======================================================
 
     def show(self):
-
         self.visible = True
 
         self.set_progress(
@@ -220,7 +185,6 @@ class LoadingScreen:
     # ======================================================
 
     def hide(self):
-
         self.visible = False
 
         self.background.hide()
@@ -232,5 +196,4 @@ class LoadingScreen:
     # ======================================================
 
     def destroy(self):
-
         self.background.destroy()
